177778e2a120c98a59751caca24019e18e3625de
Samuel Wanjohi <sam.wanjohi94@gmail.com>
2024-12-11 10:59:40 +0300

    Add project files.

---
+++ ExceptionUtils.cs
@@ -0,0 +1,30 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="ExceptionUtils.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+using System.Diagnostics;
+
+namespace NodeToStringBuilderBracketsBug;
+
+internal class ExceptionUtils
+{
+    [DebuggerStepThrough]
+    internal static T CheckArgumentNotNull<T>([ValidatedNotNull] T value, string parameterName) where T : class
+    {
+        Debug.Assert(!string.IsNullOrEmpty(parameterName), "!string.IsNullOrEmpty(parameterName)");
+
+        if (value == null)
+        {
+            throw new ArgumentNullException(parameterName);
+        }
+
+        return value;
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter)]
+    private sealed class ValidatedNotNullAttribute : Attribute
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.OData.Edm;
+using Microsoft.OData.UriParser;
+using NodeToStringBuilderBracketsBug;
+
+// Define the EDM model
+var model = EdmModelBuilder.TestModel;
+
+// Define the OData query string
+string text = "a sub (b sub c) eq 1";
+
+// Parse and translate the OData query string
+var parser = new ODataQueryOptionParser(
+    model,
+    EdmModelBuilder.GetEntityType(),
+    EdmModelBuilder.GetEntitySet(),
+    new Dictionary<string, string>() { { "$filter", text } });
+
+var filterClause = parser.ParseFilter();
+
+if (filterClause.Expression is BinaryOperatorNode binaryOperatorNode)
+{
+    NodeToStringBuilder visitor = new NodeToStringBuilder();
+    var result = visitor.Visit(binaryOperatorNode);
+
+    Console.WriteLine(result);
+}
+
+public class EdmModelBuilder
+{
+    public static IEdmModel TestModel = GetEdmModel();
+
+    private static IEdmModel GetEdmModel()
+    {
+        var model = new EdmModel();
+
+        var entityType = new EdmEntityType("NS", "Entity");
+        entityType.AddKeys(entityType.AddStructuralProperty("ID", EdmPrimitiveTypeKind.Int32));
+        entityType.AddStructuralProperty("a", EdmPrimitiveTypeKind.Int32);
+        entityType.AddStructuralProperty("b", EdmPrimitiveTypeKind.Int32);
+        entityType.AddStructuralProperty("c", EdmPrimitiveTypeKind.Int32);
+
+        var entityContainer = new EdmEntityContainer("NS", "Container");
+        var entitySet = entityContainer.AddEntitySet("Entities", entityType);
+        model.AddElement(entityType);
+        model.AddElement(entityContainer);
+
+        return model;
+    }
+
+    public static IEdmEntityType? GetEntityType()
+    {
+        return TestModel.FindType("NS.Entity") as IEdmEntityType;
+    }
+
+    public static IEdmEntitySet GetEntitySet()
+    {
+        return TestModel.FindEntityContainer("Container").FindEntitySet("Entities");
+    }
+}
